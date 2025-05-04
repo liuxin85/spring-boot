4a7e82ea34b11a565efc0f7bac591f89a1bb74d5
liuxin85 <liuxin85@gmail.com>
2025-05-04 21:36:38 +0800

    Implementing a user registration service

---
+++ src/main/java/com/example/store/User.java
@@ -0,0 +1,29 @@
+package com.example.store;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+
+    public User(Long id, String email, String password, String name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
