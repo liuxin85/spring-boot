c935340f558e64da89fc1da85dfff28acebce595
liuxin85 <liuxin85@gmail.com>
2025-05-04 21:36:48 +0800

    Implementing a user registration service

---
+++ src/main/java/com/example/store/StoreApplication.java
@@ -0,0 +1,20 @@
+package com.example.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class StoreApplication {
+
+    public static void main(String[] args) {
+
+       ApplicationContext context =  SpringApplication.run(StoreApplication.class, args);
+       var userService = context.getBean(UserService.class);
+       userService.registerUser(new User(1L, "mosh@codewithmosh.com", "12345", "Mosh"));
+       userService.registerUser(new User(1L, "mosh@codewithmosh.com", "12345", "Mosh"));
+
+    }
+
+}
